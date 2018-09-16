07fd7f519517a301a0d648bbe396486823825c6e
Kyle <kyle.rozanitis@gmail.com>
2018-09-16 15:21:30 -0400

    Added data_parser to app.py

--- app.py
+++ app.py
@@ -14,33 +14,55 @@
 
 # Library imports
 from prettytable import PrettyTable
+import os
+from "class.py" import individualPerson
 
+individual_data = dict()
+family_data = dict()
 
 def read_data_file(file_name):
-    """Read GEDCOM files Strip data into a tuple of list"""
+    """Read GEDCOM file & strip data into a tuple of lists"""
+    filename = os.fsdecode(file_name)
+    if filename.endswith(".ged"):
+        try:
+            open(filename, "r")
+        except FileNotFoundError:
+            print(filename, "file cannot be opened!")
+        except IOError:
+            print('Please check that file is not corrupted.')
+        else:
+            # if file opens successfully
+            with open(filename, 'r') as file_opened:
+                # reading data from file
+                each_line = file_opened.readlines()
+                if len(each_line) == 0:
+                    raise ValueError('{} is empty'.format(file_name))
+                else:
+                    data_values = tuple([e.strip('\n').split(' ') for e in each_line])
+                return data_values
 
-    try:
-        open(file_name, "r")
 
-    except FileNotFoundError:
-        print(file_name, "file cannot be opened!")
-
-    except IOError:
-        print('Please check that file is not corrupted.')
-
-    else:
-        # if file opens successfully
-        with open(file_name, 'r') as file_opened:
-            # reading data from file
-            each_line = file_opened.readlines()
-
-
-            if len(each_line) == 0:
-                raise ValueError('{} is empty'.format(file_name))
-            else:
-                data_values = tuple([e.strip('\n').split(' ') for e in each_line])
-
-            return data_values
+def data_parser(data):
+    current_individual = ""
+    current_tag = ""
+    for item in data:
+        if item[0] == "0" and len(item) < 3:
+            continue
+        elif item[0] == "0" and item[2] == "INDI":
+            current_individual = item[1]
+            individual_data[current_individual] = individualPerson(current_individual)
+        elif item[0] == "1" and item[1] in ["NAME", "SEX", "FAMC", "FAMS"]:
+            i1 = individual_data[current_individual]
+            current_tag = item[1].lower()
+            i1.current_tag = item[2:]
+        elif item[0] == "1" and item[1] == ["BIRT", "DEAT"]:
+            current_tag = item[1].lower()
+            i1 = individual_data[current_individual]
+            if current_tag == "DEAT":
+                i1.alive = False
+        elif item[0] == "2" and item[1] == "DATE":
+            i1 = individual_data[current_individual]
+            i1.current_tag = item[2:]
 
 
 def create_table():
@@ -68,9 +90,9 @@ def main():
     """Main Function program Execution"""
 
     tbl = create_table()
-    print(read_data_file('My_Family.ged'))
+    raw_data = read_data_file('My_Family.ged')
+    data_parser(raw_data)
 
 
 if __name__ == '__main__':
     main()
-
--- class.py
+++ class.py
@@ -1,20 +1,25 @@
-class individualPerson(object):
+""" This file takes contains classes for the Indivual and Family. """
 
-    def __init__(self, uid,name,birthday,sex):
+
+class individualPerson(object):
+    """ This class uses the user's ID (UID), name, birthday, and sex to create
+    an instance of an individual. """
+    def __init__(self, uid):
         self.uid = uid  #umoque id of individual person
-        self.name = name # name of individual person
-        self.birthday = birthday # Date of birthday of individual person
-        self.sex = sex # sex of individual person
-        self.deathDate = 'NA' # Date of death of individual person
+        self.name = "NA" # name of individual person
+        self.birt = "NA" # Date of birthday of individual person
+        self.sex = "NA" # sex of individual person
+        self.deat = "NA" # Date of death of individual person
         self.alive = True # person alive or dead
         self.famc = [] # family id where individual is a child
         self.fams = [] # family id where individual is parent
 
-# class for families
-class familyClass(object):
 
-    def __init__(self, uid):
-        self.uid = uid
+class familyClass(object):
+    """ This class uses the family's ID (FID) to store information about a
+    particular family. """
+    def __init__(self, fid):
+        self.fid = fid
         self.marriage = 'NA'  # marriage event for family
         self.husband = 'NA'  # for husband in family
         self.husbandName = 'NA' # name of husband
