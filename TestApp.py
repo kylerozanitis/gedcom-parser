80d7e9dbcc4a4091c6318cc20b0cd3d4d1b5de76
latinocodes <joselara02@outlook.com>
2018-09-17 13:29:48 -0400

    removing unittest of the function deleted

--- TestApp.py
+++ TestApp.py
@@ -1,14 +1,8 @@
-from app import create_table_individual, read_data_file, data_parser
-from classes import individualPerson, familyClass
 import unittest
 
 class TestTable(unittest.TestCase):
-
-    def test_create_table_individual(self):
-        y = create_table_individual([])
-        x = create_table_individual([])
-        self.assertEqual(x, y, True)
-
+    """Unit test will be define here"""
+    pass
 
 if __name__ == '__main__':
     print('Running unit tests')
