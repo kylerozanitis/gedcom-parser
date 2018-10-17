fb4b35407a7f81432589ddfc3aa1251cbbddf10c
ranakshiti <krana4@stevens.edu>
2018-10-17 14:07:40 -0400

    working on US32

--- helperFunctions_Sprint3.py
+++ helperFunctions_Sprint3.py
@@ -1,5 +1,6 @@
 from helperFunctions_Sprint1 import validate_date_format, change_date_format, check_two_dates
 from helperFunctions_Sprint1 import convert_str_to_date, print_both
+from collections import Counter
 
 def single_over_30(family_data, individual_data):
     """ US31 - List all single over 30
@@ -21,3 +22,26 @@ def single_over_30(family_data, individual_data):
                 single.append(individual)
     return single
 
+def multiple_births(family_data, individual_data):
+    """
+    US32 - List multiple births
+    This function will take family data and individual data as input
+    and list all the children who are born on same day from one family.
+    """
+    multiple_births = []
+
+    for family in family_data.values():
+        children = family.chil
+        #print('child:',children)
+        siblings = []
+        for individual in individual_data.values():
+            if individual.uid in children:
+                siblings.append(individual)
+        #print(len(siblings))
+        #print(siblings)
+        siblings_birth = []
+        for s in siblings:
+            siblings_birth.append(s.birt)
+        #print('birth',siblings_birth)
+        
+                
