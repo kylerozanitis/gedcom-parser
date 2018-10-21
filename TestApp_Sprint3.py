23c8a0d5d1785b4168c1f4a599bdba7ff7dead02
latinocodes <joselara02@outlook.com>
2018-10-21 18:19:25 -0400

    pulling

--- TestApp_Sprint3.py
+++ TestApp_Sprint3.py
@@ -1,6 +1,6 @@
 import unittest
 from classes import individualPerson, familyClass
-from helperFunctions_Sprint3 import single_over_30, siblings_should_not_marry, get_spouse, get_children
+from helperFunctions_Sprint3 import single_over_30
 
 class TestHelperFunctions(unittest.TestCase):
     """Unit for HelperFunction File"""
@@ -39,50 +39,6 @@ class TestHelperFunctions(unittest.TestCase):
         fam_dict[family.fid] = family
 
         self.assertEqual(len(single_over_30(fam_dict, ind_dict)), 2, True)
-
-    def test_siblings_should_not_marry(self):
-        fam_dict = {}
-        ind_dict = {}
-
-        individual = individualPerson("I1")
-        individual.uid = "I1"
-        individual.fams = ['F5']
-        individual.famc = ["F4"]
-        ind_dict[individual.uid] = individual
-
-        individual2 = individualPerson("I2")
-        individual2.uid = "I2"
-        individual2.fams = ['F2']
-        individual2.famc = ["F4"]
-        ind_dict[individual2.uid] = individual2
-
-        family = familyClass("F1")
-        family.husb_id = "I1"
-        family.wife_id = "I2"
-        family.chil = ["I3", "I4"]
-        fam_dict[family.fid] = family
-
-        self.assertEqual(get_spouse(fam_dict, "F1", "I1"), "I2", True)
-        self.assertEqual(get_spouse(fam_dict, "F1", "I2"), "I1", True)
-
-        individual3 = individualPerson("I3")
-        individual3.fams = ['F5']
-        individual3.famc = ["F4"]
-        ind_dict[individual3.uid] = individual3
-
-        individual4 = individualPerson("I4")
-        individual4.fams = ['F2']
-        individual4.famc = ["F4"]
-        ind_dict[individual4.uid] = individual4
-
-        family = familyClass("F2")
-        family.husb_id = "I1"
-        family.wife_id = "I2"
-        family.chil = ["I1", "I2"]
-        fam_dict[family.fid] = family
-
-        self.assertEqual(get_children(fam_dict, "F2"), ["I1", "I2"], True)
-        self.assertEqual(len(siblings_should_not_marry(fam_dict, ind_dict)), 2, True)
 
 
 
